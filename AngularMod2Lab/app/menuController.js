8fcf7f070fa50b0319ad194cf7c49fa70a59a5d9
Carolina Martes <carolina.martes@gmail.com>
2016-11-15 16:52:45 -0500

    Add Angular labs

---
+++ AngularMod2Lab/app/menuController.js
@@ -0,0 +1,17 @@
+app.controller('menuController', [
+    '$scope',
+    function($scope) {
+        $scope.model = { title: 'Our Menu' };
+        $scope.changeMainDish = function(item) {
+            $scope.model.mainDish = {
+                item: item,
+                price: "$13.99"
+            };
+            $scope.$watch('model.mainDish', function(newValue, oldValue) {
+                if (newValue === 'BBQ Chicken Pizza') {
+                    alert('You have selected the BBQ Chicken Pizza!');
+                }
+            });
+        }
+    }
+]);
---
+++ AngularMod3Lab/app/pizzaController.js
@@ -0,0 +1,16 @@
+app.controller('pizzaController', [
+    '$scope',
+    function($scope) {
+        $scope.model = {
+            title: 'Pizza Builder',
+            availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'],
+            toppings: [],
+            message: "Topping Added"
+        };
+        $scope.addTopping = function(topping) {
+            $scope.model.toppings.push(topping);
+            $scope.model.search = null;
+            $scope.model.showAdded = true
+        }
+    }
+]);
